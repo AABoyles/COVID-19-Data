ea5548c2cd16aca471a4724b330695caca2fa633
Tony Boyles <AABoyles@gmail.com>
2020-03-18 15:24:30 -0400

    Update Citizen Times data It's messy!

--- scripts/scrape-citizen-times.js
+++ scripts/scrape-citizen-times.js
@@ -5,16 +5,20 @@ const url = "https://data.citizen-times.com/media/jsons/smpj/covid19_r2.json";
 
 axios.get(url).then(response => {
   fs.writeFileSync('citizen-times/raw.json', JSON.stringify(response.data));
-  let csv = Papa.unparse(response.data.features.map(f => ({
-    "Country": f.properties.n,
-    "Confirmed": f.properties.c,
-    "Deaths": f.properties.d,
-    "Recovered": f.properties.r,
-    "Active": f.properties.e,
-    "Timestep": f.properties.t, 
-    "Date": f.properties.a
-  })));
+  let csv = Papa.unparse(response.data.features.map(f => {
+    let [Country, Province, Location, ...err] = f.properties.n.split(',').reverse().map(s => s.trim());
+    Country = Country == "Mainland China" ? "China" : Country;
+    return {
+      Country,
+      Province,
+      Location,
+      "Confirmed": f.properties.c,
+      "Deaths": f.properties.d,
+      "Recovered": f.properties.r,
+      "Active": f.properties.e,
+      "Timestep": f.properties.t, 
+      "Date": (new Date(f.properties.a + ' 2020')).toISOString().substr(0,10)
+    }
+  }));
   fs.writeFileSync('citizen-times/data.csv', csv);
-});
-
-//The file is saved at `citizen-times.csv`
+}).catch(e => console.error(e));
